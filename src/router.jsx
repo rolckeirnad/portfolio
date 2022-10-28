a1f5b64cf84add7ae81cfa067dfcaa92529cc03f
Daniel Gallardo <dgallardo.c@outlook.com>
2022-10-28 02:15:43 -0500

    Add Home component

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom';
 
 import App from './App';
+import Home from './routes/Home';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -10,7 +11,7 @@ const router = createBrowserRouter(
       path="/"
       element={<App />}
     >
-      <Route path="" element={<div className="col">About Me page</div>} />
+      <Route path="" element={<Home />} />
       <Route path="portfolio" element={<div className="col">Portfolio page</div>} />
       <Route path="resume" element={<div className="col">Resume page</div>} />
       <Route path="contact" element={<div className="col">Contact page</div>} />
