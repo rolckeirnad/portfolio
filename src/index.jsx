fe6d1cd0528ab469eeac92c569645548bcb2102e
Daniel Gallardo <dgallardo.c@outlook.com>
2022-10-28 21:47:29 -0500

    Add QueryClient

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from './router';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
