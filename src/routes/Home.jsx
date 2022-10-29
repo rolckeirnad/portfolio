6959402f0464e482f6805f7ff6a903bbb186a3cd
Daniel Gallardo <dgallardo.c@outlook.com>
2022-10-28 20:05:54 -0500

    Add About Me section

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -11,14 +11,10 @@ function Home() {
         </div>
         <div className="col-lg-6">
           <h1 className="display-5 fw-bold lh-1 px-1">Daniel Gallardo</h1>
-          <h2 className="display-6 lh-1 px-1">Front End Jr. Developer</h2>
-          <p className="lead">
-            My name is Daniel Gallardo and I&apos;m learning Web Development.
-
-            Previously worked as an electrician mechanic and electronics technician.
-
-            I&apos;m interested in programming, electronics and embedded devices. I&apos;ve
-            worked with Arduino and Metro pyboard w/MicroPython.
+          <h2 className="display-6 lh-1 px-1">Frontend Jr. Developer</h2>
+          <p className="fs-4">
+            I&apos;m an autodidact person who likes to solve problems. Currently, I&apos;m focused
+            on kickstarting a career in web development
           </p>
           <div className="d-grid gap-2 d-md-flex justify-content-md-start">
             <button type="button" className="btn btn-primary btn-lg px-4"><Link to="portfolio" className="nav-link text-white">Portfolio</Link></button>
@@ -26,7 +22,62 @@ function Home() {
           </div>
         </div>
       </div>
-      <div>Learned Technologies</div>
+      <div className="d-flex flex-column gap-1">
+        <div>
+          <h2 className="display-6 fw-bold lh1 px-1">About me</h2>
+          <p className="styled-links fs-4 px-4">
+            My name is Daniel and I started this journey just before pandemic.
+            I applied for a scholarship at a recognized bootcamp but unfortunately it was rejected.
+            During pandemic I had temporarily home office work, so I used the time saved from my
+            commutes to start learning by myself.
+
+            I started with
+            {' '}
+            <a href="https://www.freecodecamp.org/">freeCodeCamp</a>
+            {' '}
+            course and learnt HTML, CSS and JavaScript.
+
+            Currently I&apos;m taking a refresher and following a learning path from
+            {' '}
+            <a href="https://www.theodinproject.com/">
+              The Odin
+              Project.
+            </a>
+
+            Below I listed all technologies that I&apos;ve been working recently.
+          </p>
+        </div>
+        <h2 className="display-6 fw-bold lh1 px-1">Technical Skills</h2>
+        <h3 className="mt-4">Learned Skills</h3>
+        <div className="badge-container">
+          <img className="badge-img" src="https://img.shields.io/badge/JavaScript-black?style=for-the-badge&logo=javascript" alt="javascript" />
+          <img className="badge-img" src="https://img.shields.io/badge/HTML5-D9D9D9?style=for-the-badge&logo=html5" alt="html5" />
+          <img className="badge-img" src="https://img.shields.io/badge/CSS3-016ab5?style=for-the-badge&logo=css3" alt="css3" />
+        </div>
+        <h3 className="mt-4">Tools</h3>
+        <div className="badge-container">
+          <img className="badge-img" src="https://img.shields.io/badge/VS_Code-black?style=for-the-badge&logo=visualstudiocode" alt="VS code" />
+          <img className="badge-img" src="https://img.shields.io/badge/npm-black?style=for-the-badge&logo=npm" alt="npm" />
+          <img className="badge-img" src="https://img.shields.io/badge/Webpack-black?style=for-the-badge&logo=webpack" alt="webpack" />
+          <img className="badge-img" src="https://img.shields.io/badge/Git-black?style=for-the-badge&logo=git" alt="git" />
+          <img className="badge-img" src="https://img.shields.io/badge/GitHub-black?style=for-the-badge&logo=github" alt="github" />
+        </div>
+        <h3 className="mt-4">Currently Learning</h3>
+        <div className="badge-container">
+          <img className="badge-img" src="https://img.shields.io/badge/react-%2320232a?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="React" />
+          <img className="badge-img" src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white" alt="React Router" />
+          <img className="badge-img" src="https://img.shields.io/badge/Bootstrap-%23563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" alt="Bootstrap" />
+          <img className="badge-img" src="https://img.shields.io/badge/Sass-FF69B4?style=for-the-badge&logo=sass&logoColor=white" alt="Sass" />
+        </div>
+      </div>
+      <div className="py-5 text-center">
+        <h2 className="display-6 fw-bold lh1 px-1">Portfolio</h2>
+        <p className="fs-4">
+          You can check my portfolio with all my projects
+          hosted on GitHub pages clicking on the following button
+        </p>
+        <button type="button" className="btn btn-primary bt-lg px-4"><Link to="portfolio" className="nav-link text-white">Check my portfolio</Link></button>
+      </div>
     </div>
   );
 }
