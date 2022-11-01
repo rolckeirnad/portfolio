475d17e2ac6ace307129a0d7d7b025f879c8a4d9
Daniel Gallardo <dgallardo.c@outlook.com>
2022-10-31 20:29:05 -0600

    Add preview image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,48 @@
+/* eslint-disable react/prop-types */
 import {
   formatDistance, parseISO, startOfToday,
 } from 'date-fns';
-import React from 'react';
+import React, { useState } from 'react';
+import placeholder from '../assets/images/rolckeirnad.png';
+
+const loadImage = async (image) => new Promise((resolve, reject) => {
+  const loadImg = new Image();
+  loadImg.src = image;
+  loadImg.onload = () => resolve(image);
+  loadImg.onerror = (err) => reject(new Error(err));
+});
 
 function Card({ info }) {
+  const [loaded, setLoaded] = useState(false);
+  const [url, setUrl] = useState();
   const {
-    id, name, description, pushed_at, updated_at, topics,
+    id, name, description, pushed_at, topics,
   } = info;
+
+  loadImage(`https://raw.githubusercontent.com/rolckeirnad/${name}/main/preview.png`)
+    .then((image) => {
+      setUrl(image);
+    })
+    .catch((err) => {
+      console.error(`${name} repo do not has preview image`, err);
+      setUrl(placeholder);
+    })
+    .finally(() => {
+      setLoaded(true);
+    });
+
   return (
     <div className="card h-100 text-center text-bg-light" key={id} id={id}>
-      <div className="overflow-hidden">
-        <a href={`https://rolckeirnad.github.io/${name}`} target="_blank" rel="noreferrer noopener">
-          <img
-            src="http://placekitten.com/200/300"
-            className="card-img-top card-repo-image"
-            alt="kitty"
-          />
-        </a>
+      <div className="card-header d-flex justify-content-center align-items-center" style={{ height: '200px' }}>
+        <div className={loaded ? 'overflow-hidden h-100 w-100' : 'spinner-border'}>
+          <a href={`https://rolckeirnad.github.io/${name}`} target="_blank" rel="noreferrer noopener">
+            <img
+              src={url}
+              className={loaded ? 'card-img-top card-repo-image' : 'visually-hidden'}
+              alt="kitty"
+            />
+          </a>
+        </div>
       </div>
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
